4db986d82d8a1aa24d44f3bb4cc30a3053cdb3b9
boukharSoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-11-23 13:39:49 +0100

    test generator

--- app/database/seeders/UserSeeder.php
+++ app/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -19,21 +20,21 @@ class UserSeeder extends Seeder
             [
                 'name' => 'admin',
                 'email' => 'admin@gmail.com',
-                'password'=>'admin',
+                'password'=>Hash::make('admin'),
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
                 'name' => 'service social',
                 'email' => 'social@gmail.com',
-                'password'=>'social',
+                'password'=>Hash::make('social'),
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
             [
                 'name' => 'Medecin générale',
                 'email' => 'medecin@gmail.com',
-                'password'=>'medecin',
+                'password'=>Hash::make('medecin'),
                 'created_at' => $now,
                 'updated_at' => $now,
             ],
